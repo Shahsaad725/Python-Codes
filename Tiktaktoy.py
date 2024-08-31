4e0006423047cfd02219855eb1049e035d2b19e0
Shah Saad <shahsaaddotani@gmail.com>
2024-08-31 03:12:05 -0700

    Add files via upload

--- Tiktaktoy.py
+++ Tiktaktoy.py
@@ -6,9 +6,9 @@ import winsound as wi
 
 
 global replay  #______taken globally so that can be used in each function and can be updated correctly in button 
-replay = 1
+replay = True
 
-while replay ==1:       # for retrying
+while replay == True:       # for retrying
     screen = t.Screen()
     screen = t.setup(width=600, height=800)
     screen = t.bgpic("Bg_pic/Gif Format/view-illuminated-neon-gaming-keyboard-setup-controller.gif")
@@ -16,115 +16,67 @@ while replay ==1:       # for retrying
 
 
     pen1 = t.Turtle()
-    pen1 = t.pencolor("white")
+    
+    def draw_Boxes():
+        pen1 = t.pencolor("white")
 
-    pen_2 = t.Turtle()
+        pen_2 = t.Turtle()
 
-    pen1 = t.width(5)
-    pen1 = t.speed(10)
-    pen1 = t.hideturtle()
+        pen1 = t.width(5)
+        pen1 = t.penup()
+        pen1 = t.goto(0,0)
+        pen1 = t.pendown()        
+        pen1 = t.speed(10)
+        pen1 = t.hideturtle()
 
-    pen1 = t.backward(200)
-    pen1 = t.forward(400)
-    pen1 = t.backward(150)
-    pen1 = t.left(90)
-    pen1 = t.forward(150)
-    pen1 = t.backward(400)
-    pen1 = t.forward(250)
-    pen1 = t.left(90)
-    pen1 = t.forward(100)
-    pen1 = t.right(90)
-    pen1 = t.forward(150)
-    pen1 = t.backward(400)
-    pen1 = t.forward(150)
-    pen1 = t.right(90)
-    pen1 = t.backward(150)
-    pen1 = t.forward(400)
+        pen1 = t.backward(200)
+        pen1 = t.forward(400)
+        pen1 = t.backward(150)
+        pen1 = t.left(90)
+        pen1 = t.forward(150)
+        pen1 = t.backward(400)
+        pen1 = t.forward(250)
+        pen1 = t.left(90)
+        pen1 = t.forward(100)
+        pen1 = t.right(90)
+        pen1 = t.forward(150)
+        pen1 = t.backward(400)
+        pen1 = t.forward(150)
+        pen1 = t.right(90)
+        pen1 = t.backward(150)
+        pen1 = t.forward(400)
 
-
-
-    #_______________________________Box drawning is ended________________________________________
-
+    draw_Boxes()
 
     # ______________________________Naming the Quadrants_________________________________________
 
+    def typing_sound(duration,frequency):
+        #  duration is the period of sounding
+        # frequency is of the sound
+        wi.Beep(frequency,int(duration*1000))
 
 
-
-
-    # ______________________________Quadrant 1________________________________________________________
-    pen1 = t.penup()
-    pen1 = t.goto(-110,60)
-    pen1 = t.pendown()
-    q1   = t.write("Q : 1", align="center", font=("Arial",24,"italic"))
-    pen1 = t.hideturtle()
-
-    # ______________________________Quadrant 2________________________________________________________
-    pen1 = t.penup()
-    pen1 = t.goto(0,60)
-    pen1 = t.pendown()
-    q2   = t.write("Q : 2", align="center", font=("Arial",24,"italic"))
-    pen1 = t.hideturtle()
-
-
-    # ______________________________Quadrant 3________________________________________________________
-    pen1 = t.penup()
-    pen1 = t.goto(110,60)
-    pen1 = t.pendown()
-    q3   = t.write("Q : 3", align="center", font=("Arial",24,"italic"))
-    pen1 = t.hideturtle()
-
-
-    # ______________________________Quadrant 4________________________________________________________
-    pen1 = t.penup()
-    pen1 = t.goto(-110,-60)
-    pen1 = t.pendown()
-    q4   = t.write("Q : 4", align="center", font=("Arial",24,"italic"))
-    pen1 = t.hideturtle()
-
-
-    # ______________________________Quadrant 5________________________________________________________
-
-    pen1 = t.penup()
-    pen1 = t.goto(0,-60)
-    pen1 = t.pendown()
-    q5   = t.write("Q : 5", align="center", font=("Arial",24,"italic"))
-    pen1 = t.hideturtle()
-
-
-    # ______________________________Quadrant 6________________________________________________________
-    pen1 = t.penup()
-    pen1 = t.goto(110,-60)
-    pen1 = t.pendown()
-    q6   = t.write("Q : 6", align="center", font=("Arial",24,"italic"))
-    pen1 = t.hideturtle()
-
-
-    # ______________________________Quadrant 7________________________________________________________
-
-    pen1 = t.penup()
-    pen1 = t.goto(-110,-180)
-    pen1 = t.pendown()
-    q7   = t.write("Q : 7", align="center", font=("Arial",24,"italic"))
-    pen1 = t.hideturtle()
-
-
-    # ______________________________Quadrant 8________________________________________________________
-    pen1 = t.penup()
-    pen1 = t.goto(0,-180)
-    pen1 = t.pendown()
-    q8   = t.write("Q : 8", align="center", font=("Arial",24,"italic"))
-    pen1 = t.hideturtle()
-
-
-    # ______________________________Quadrant 9________________________________________________________
-    pen1 = t.penup()
-    pen1 = t.goto(110,-180)
-    pen1 = t.pendown()
-    q9   = t.write("Q : 9", align="center", font=("Arial",24,"italic"))
-    pen1 = t.hideturtle()
-
-
+    # ______________________________Naming_Quadrant________________________________________________________
+    def Name_Quadrants():
+        x = -110
+        y = 60
+        for i in range(1,10,1):
+            pen1 = t.speed(10)
+                       
+            pen1 = t.penup()
+            pen1 = t.goto(x,y)
+            pen1 = t.pendown()
+            q1   = t.write(f"Q :{i}", align="center", font=("Arial",24,"italic"))
+            typing_sound(duration=0.3,frequency=2000)
+            pen1 = t.hideturtle()
+            x += 110
+            
+            if i % 3 == 0:
+                y -= 120
+                x -= 3*110
+    
+    
+    Name_Quadrants()        
 
     # _________________________________Alert the user to use the quadrant name for play____________________
 
@@ -132,7 +84,7 @@ while replay ==1:       # for retrying
 
     pen = t.Turtle()
     pen.pensize(5)
-    pen.speed(1)
+    pen.speed(7)
     pen.hideturtle()
     pen.color("white")
     x_axis = -630
@@ -141,10 +93,7 @@ while replay ==1:       # for retrying
     pen.goto(x_axis,y_axis)
     pen.down()
 
-    def typing_sound(duration=0.01,frequency=1500):
-        #  duration is the period of sounding
-        # frequency is of the sound
-        wi.Beep(frequency,int(duration*1000))
+
 
 
     def alert(i):
@@ -263,7 +212,7 @@ while replay ==1:       # for retrying
         pen.down()
         pen.write(masg, font=("Arial", 24 ,"italic")) 
         if masg!=' ':
-            typing_sound(duration=0.1,frequency=1500)
+            typing_sound(duration=0.06,frequency=1500)
         x_axis = x_axis+25
 
 
@@ -278,28 +227,7 @@ while replay ==1:       # for retrying
 
     # __________________________________Redrawing the boxes after clearing the quadrants name__________________
 
-    pen1 = t.width(5)
-    pen1 = t.penup()
-    pen1 = t.goto(0,0)
-    pen1 = t.pendown()
-    pen1 = t.speed(10)
-
-    pen1 = t.backward(200)
-    pen1 = t.forward(400)
-    pen1 = t.backward(150)
-    pen1 = t.left(90)
-    pen1 = t.forward(150)
-    pen1 = t.backward(400)
-    pen1 = t.forward(250)
-    pen1 = t.left(90)
-    pen1 = t.forward(100)
-    pen1 = t.right(90)
-    pen1 = t.forward(150)
-    pen1 = t.backward(400)
-    pen1 = t.forward(150)
-    pen1 = t.right(90)
-    pen1 = t.backward(150)
-    pen1 = t.forward(400)
+    draw_Boxes()
 
     
 
@@ -314,6 +242,9 @@ while replay ==1:       # for retrying
     winner_2 = []
     winner = []
     position = 0
+    check_overwrite = []
+    win_line = []
+
 
 
     # Initialize the turtles
@@ -325,7 +256,7 @@ while replay ==1:       # for retrying
     def get_quadrant_no(k):
         window = tk.Tk()
         window.withdraw()  # Hide the root window
-        q_no = simpledialog.askinteger("Input", f"Player {k % 2 +1}, Enter an integer (1-9):")
+        q_no = simpledialog.askinteger("Input", f"Player {k % 2 +1}, Enter an integer (1-9):\nTry NO # {k+1}")
         window.destroy()  # Destroy the root window
         return q_no
 
@@ -337,41 +268,54 @@ while replay ==1:       # for retrying
 
         Q_No = get_quadrant_no(k)
         
-        if Q_No not in range(1, 10):
-            print("Invalid quadrant number, skipping...")
-            continue
         
-        labels = {
-            1: (-110, 60),
-            2: (0, 60),
-            3: (110, 60),
-            4: (-110, -60),
-            5: (0, -60),
-            6: (110,-60),
-            7: (-110, -160),
-            8: (0, -160),
-            9: (110, -160)
-        }
-        
-        x, y = labels.get(Q_No, (0, 0))
-        
-        pen1.penup()
-        pen1.goto(x, y)
-        pen1.pendown()
-        pen1.pencolor("magenta" if k % 2 == 0 else "cyan")
-        pen1.circle(10)
-        pen1.hideturtle()
+        if Q_No not in check_overwrite:
 
-        if k % 2 == 0:
-            if k >= len(winner_1): #_______IF the indices are lesser, then create more indices____________
-                winner_1.extend([None]*(k+1- len(winner_1)))
-                
-            winner_1[k] = Q_No
+            
+            check_overwrite.append(Q_No)
+            
+            if Q_No not in range(1, 10):
+                print("Invalid quadrant number, skipping...")
+                k = k - 1               #___It makes the loop to iterate thruogh 9 times even when the continue statement is run
+                continue
+            
+            labels = {
+                1: (-110, 60),
+                2: (0, 60),
+                3: (110, 60),
+                4: (-110, -60),
+                5: (0, -60),
+                6: (110,-60),
+                7: (-110, -160),
+                8: (0, -160),
+                9: (110, -160)
+            }
+            
+            x, y = labels.get(Q_No, (0, 0))
+            
+            pen1.penup()
+            pen1.goto(x, y)
+            pen1.pendown()
+            pen1.pencolor("magenta" if k % 2 == 0 else "cyan")
+            typing_sound(duration=0.1,frequency=4000)
+            pen1.circle(10)
+            pen1.hideturtle()
+
+            if k % 2 == 0:
+                if k >= len(winner_1): #_______IF the indices are lesser, then create more indices____________
+                    winner_1.extend([None]*(k+1- len(winner_1)))
+                    
+                winner_1[k] = Q_No
+            else:
+                if k >= len(winner_2): #_______IF the indices are lesser, then create more indices____________
+                    winner_2.extend([None]*(k+1- len(winner_2)))        
+                winner_2[k] = Q_No
+
         else:
-            if k >= len(winner_2): #_______IF the indices are lesser, then create more indices____________
-                winner_2.extend([None]*(k+1- len(winner_2)))        
-            winner_2[k] = Q_No
-
+            print("The Quadrant is already used:")
+            k = k - 1            #___It makes the loop to iterate thruogh 9 times even when the continue statement is run
+            continue
+            
     # __________________________________________Decleraing The Winner____________________________________
         
         def alert(i):
@@ -602,6 +546,7 @@ while replay ==1:       # for retrying
                 if position is not None:
                     x,y = position    
                     pen1.down()
+                    typing_sound(duration=0.1,frequency=37)
                     pen1.goto(x,y)
     
                 
@@ -609,7 +554,7 @@ while replay ==1:       # for retrying
 
         
         def winner (player_tik):
-            winning_combination = [                           #_________Array Type________________________ 
+            winning_combination = [                        #_________Array Type________________________ 
                                 {1,2,3},{4,5,6},{7,8,9},   #_________Row
                                 {1,4,7},{2,5,8},{3,6,9},   #_________Column
                                 {1,5,9},{3,5,7}            #_________Diagonal
@@ -618,6 +563,7 @@ while replay ==1:       # for retrying
             player_set = set(player_tik)      #_____A set(math) of Player_tik is made and then assigned to player_set
             for comb in winning_combination:
                 if comb.issubset(player_set):
+                    win_line = comb
                     return True
             return False     
                 
@@ -627,6 +573,7 @@ while replay ==1:       # for retrying
                     pen1.up()
                     winner = set(winner_1)
                     winning_line(winner)
+                    # winning_line(win_line)
                     # for i in winner_1:
                     #     x,y = labels.get(Q_No)
                     #     pen1.down()
@@ -671,7 +618,7 @@ while replay ==1:       # for retrying
             
                     winner = set(winner_2)
                     winning_line(winner)
-            
+                    # winning_line(win_line)
             
                     pen.penup()
                     x_axis = -600
@@ -710,28 +657,33 @@ while replay ==1:       # for retrying
     
     def assigning_true():
         global replay
-        replay = 1
+        replay = True
     def assigning_false():
         global replay
-        replay = 0
+        replay = False
     
-    main_window = tk.Tk()   # main_window = tk.Tk(): Creates the main application window. tk.Tk() initializes the Tkinter framework and creates the root window, which serves as the main container for all other GUI elements.
-    frame = tk.Frame(main_window)  # frame = tk.Frame(parent): Creates a Frame widget, which is a container used to organize other widgets. parent is the root window, so the Frame is a child of the main application window.
-    frame.pack()     # frame.pack(): Packs the Frame widget into the window using the pack geometry manager. This makes the Frame visible and positions it within the parent window.
+    def button():
+        main_window = tk.Tk()   # main_window = tk.Tk(): Creates the main application window. tk.Tk() initializes the Tkinter framework and creates the root window, which serves as the main container for all other GUI elements.
+        frame = tk.Frame(main_window)  # frame = tk.Frame(parent): Creates a Frame widget, which is a container used to organize other widgets. parent is the root window, so the Frame is a child of the main application window.
+        frame.pack()     # frame.pack(): Packs the Frame widget into the window using the pack geometry manager. This makes the Frame visible and positions it within the parent window.
 
-    replay_button = tk.Button(frame,
-                              text="Replay",
-                              foreground="green",
-                              command = assigning_true)
-    replay_button.pack(side= tk.LEFT)
+        replay_button = tk.Button(frame,
+                                text="Replay",
+                                foreground="green",
+                                command = assigning_true)
+        replay_button.pack(side= tk.LEFT)
+        
+        quit_button = tk.Button(frame,
+                                text="Quit",
+                                foreground="red",
+                                command = quit )
+        quit_button.pack(side=tk.RIGHT)
+        return replay
+        
+        
+    button = button()
     
-    quit_button = tk.Button(frame,
-                            text="Quit",
-                            foreground="red",
-                            command = quit )
-    quit_button.pack(side=tk.RIGHT)
-    
-    if replay == 1:
+    if replay == True:
        t.clear()
 
 
